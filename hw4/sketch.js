8bcc8cdf64cf27547b64b3b4efbf343ea7516b12
ling4mg9 <lingzhi@vt.edu>
2026-02-12 16:59:34 -0500

    1

--- hw4/sketch.js
+++ hw4/sketch.js
@@ -1,84 +1,76 @@
 let myShape;
 let x;
 let changeDirection;
+let wireCheck = false;
+let rl = 255;
+let gl = 0;
+let bl = 255;
 
 function preload() {
-     myShape = loadModel('tower5.obj', true);
+    myShape = loadModel("tower5.obj", true);
 }
-
 
 function setup() {
     let canvas = createCanvas(600, 400, WEBGL);
-    x=1;
-    changeDirection=false;
+    x = 1;
+    changeDirection = false;
     angleMode(DEGREES);
-   // canvas.parent("sketch-holder");
+    // canvas.parent("sketch-holder");
 }
 
 function draw() {
     fill(255);
-    ellipse(width/2,height/4,100,80);
+    ellipse(width / 2, height / 4, 100, 80);
     fill(0);
-    ellipse(width/2,height/4,20,10);
+    ellipse(width / 2, height / 4, 20, 10);
 
-    
-    
-    
     directionalLight(
- 255,85,0, // color
-  1, 1, 02 // direction
-  );
-    background(222,231,216);
+        255,
+        85,
+        0, // color
+        1,
+        1,
+        02 // direction
+    );
+    background(222, 231, 216);
     orbitControl();
-    
-    fill(91,28,12);
-    
-//圆环位置前后移动
+
+    fill(91, 28, 12);
+
+    //圆环位置前后移动
     push();
-      if(x>200){
-        changeDirection=true;}
-    
-    else if (x<=100){
-        changeDirection=false;}
-    
-    if (x>=100 && changeDirection == false){
-        x=x+1;}
-    
-    else if (changeDirection == true){
-        x=x-1;}
-    
-   translate(x,0,-100);
+    if (x > 200) {
+        changeDirection = true;
+    } else if (x <= 100) {
+        changeDirection = false;
+    }
+
+    if (x >= 100 && changeDirection == false) {
+        x = x + 1;
+    } else if (changeDirection == true) {
+        x = x - 1;
+    }
+
+    translate(x, 0, -100);
     noStroke();
-   
-      torus(90,10);
-  
-    
+
+    torus(90, 10);
+
     pop();
-    
-    fill(94,119,89);
-    stroke(91,28,12);
-    
+
+    fill(94, 119, 89);
+    stroke(91, 28, 12);
+
     model(myShape);
-    
 }
 
 function keyPressed() {
-    if (key == "z") {
-        if (gapNum >= 50) {
-            movingCheck = true;
-        } else if (gapNum <= 0) {
-            movingCheck = false;
-        }
-    } else if (key == "w") {
+    if (key == "w") {
         if (wireCheck) {
             wireCheck = false;
         } else if (!wireCheck) {
             wireCheck = true;
         }
-    } else if (key == "c") {
-        r = random(255);
-        g = random(255);
-        b = random(255);
     } else if (key == "l") {
         rl = random(255);
         gl = random(255);
